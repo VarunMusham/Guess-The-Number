6c3987f7497c54564147870164f6c95e6b138e9b
Agentalpha50 <134137752+Agentalpha50@users.noreply.github.com>
2024-07-12 14:06:09 +0530

    Update Guess the Number.py

--- Guess the Number.py
+++ Guess the Number.py
@@ -1,13 +1,22 @@
-from random import randint
+import random
 from datetime import datetime
-now=datetime.now()
-current_time=now.strftime("%H:%M:%S")
-with open('High_score.txt','r') as myfile:
-    text_data=(myfile.read())
-    text_data=text_data.split()
-    highscore=text_data[0]
-    highscore_user=text_data[2]
-    myfile.close()
+from pathlib import Path
+
+# Get the current time
+current_time = datetime.now().strftime("%H:%M:%S")
+
+# Initialize high score data
+highscore = float('inf')
+highscore_user = 'N/A'
+
+# Read the high score data
+highscore_file = Path('High_score.txt')
+if highscore_file.exists():
+    text_data = highscore_file.read_text().split()
+    if text_data:
+        highscore, highscore_user = int(text_data[0]), text_data[2]
+
+# Display welcome message and current high score
 print(f'''Welcome to Guess the Number Game.
 You are Challenged to Guess a Number b/w [0,100] and beat the Highscore of Previous User.
 When you enter a input, the compiler will be giving you tips in the following way:
@@ -16,42 +25,44 @@ Almost There - The Hidden Number is less than or equal to 10 numbers away
 You are moving Far Away - You are moving in the wrong direction.
 You are getting Closer - You are moving in the correct direction.
 Current Highscore = {highscore} by {highscore_user}''')
-user_name=input('Enter your Name:  ').title()
-x=randint(0,101)
-player_input=int(input('Enter a Number b/w [0,100]:  '))
-prev_input=player_input
-my_score=1
-while player_input!=x:
-    if (x-player_input<x-prev_input and x-player_input>0) or (player_input-x<prev_input-x and x-player_input<0):
-        print('You are getting Closer')
-    if (x-player_input>x-prev_input and x-player_input>0) or (player_input-x>prev_input-x and x-player_input<0):
-        print('You are moving Far Away')
-    if ((x-player_input>10 and x- player_input>0) or (x-player_input<-10 and x- player_input<0)) and my_score==1:
-        print('Too Far')
-    if ((x-player_input<=10 and x-player_input>0) or (x-player_input>=-10 and x-player_input<0)) and my_score==1:
-        print('Almost There')
-    prev_input=player_input
-    my_score+=1
-    player_input=int(input('Enter a New Number:  '))
-if player_input==x:
-    print(f'You have Guessed the Number in {my_score} turns')
-    if int(highscore)>my_score:
-        print(f'''Congratulations.....
-You have beaten the previous Highscore by {int(highscore)-my_score} turns.''')
-        with open('High_score.txt','w') as myfile:
-            myfile.write(f'{str(my_score)} by {user_name}')
-            myfile.close()
-    elif int(highscore)==my_score:
-        print('''Congratulations....
-You have same score as the Highscore user.''')
-        with open('High_score.txt','a') as myfile:
-            myfile.write(f'\n{str(my_score)} by {user_name}')
-            myfile.close()
-    else:
-        print(f'''You lost to previous high score by {my_score-int(highscore)} turns.
-But you have tried your best.''')
 
-with open('History.txt','a') as myfile:
-    myfile.write(f'{user_name} ---> {my_score} at {current_time}\n')
-    myfile.close()
+# Get user's name
+user_name = input('Enter your Name: ').title()
+
+# Generate random number
+hidden_number = random.randint(0, 100)
+
+# Initialize game variables
+prev_input = int(input('Enter a Number b/w [0,100]: '))
+my_score = 1
+
+# Main game loop
+while (player_input := int(input('Enter a New Number: '))) != hidden_number:
+    difference = abs(hidden_number - player_input)
+    prev_difference = abs(hidden_number - prev_input)
+    
+    print('You are getting Closer' if difference < prev_difference else 'You are moving Far Away')
+    if my_score == 1:
+        print('Too Far' if difference > 10 else 'Almost There')
+    
+    prev_input = player_input
+    my_score += 1
+
+# Player guessed the number
+print(f'You have Guessed the Number in {my_score} turns')
+
+# Update high score if necessary
+if my_score < highscore:
+    print(f'Congratulations.....\nYou have beaten the previous Highscore by {highscore - my_score} turns.')
+    highscore_file.write_text(f'{my_score} by {user_name}')
+elif my_score == highscore:
+    print('Congratulations....\nYou have same score as the Highscore user.')
+    highscore_file.write_text(highscore_file.read_text() + f'\n{my_score} by {user_name}')
+else:
+    print(f'You lost to previous high score by {my_score - highscore} turns.\nBut you have tried your best.')
+
+# Log the game history
+with open('History.txt', 'a') as history_file:
+    history_file.write(f'{user_name} ---> {my_score} at {current_time}\n')
+
 print('Thank You for trying this Game')
